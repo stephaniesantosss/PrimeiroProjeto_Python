1b74fffa5e473e44fd3c344e38f27e17eed8d1df
stephaniesantosss <stephanieps2016@hotmail.com>
2022-03-17 13:44:21 -0300

    Trabalhando com variaveis e lista

--- Listas/Variaveis_e_Listas.py
+++ Listas/Variaveis_e_Listas.py
@@ -2,11 +2,14 @@ inventario = []
 resposta = "S"
 
 while resposta == "S":
+    # append adiciona o valor a lista
     inventario.append(input("Equipamento: "))
     inventario.append(float(input("Valor: ")))
     inventario.append(int(input("Numero Serial: ")))
     inventario.append(input("Departamento: "))
+    # \"S\" é para mostrar o s com aspas e não seu valor
     resposta = input("Digite \"S\" para continuar: ").upper()
 
+# para cada elemento no inventario execute:
 for elemento in inventario:
     print(elemento)
