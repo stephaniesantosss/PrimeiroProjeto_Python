e788edf1d8f89f615a5c12195d515ab5a5c78af3
stephaniesantosss <stephanieps2016@hotmail.com>
2022-03-17 12:43:01 -0300

    Trabalhando com variaveis em python

---
+++ Var/variaveis.py
@@ -0,0 +1,17 @@
+#input sempre recebe valor como string por padrão
+nome = input("Digite um funcionário: ")
+empresa = input("Digite a instituição: ")
+#para definir o tipo da variavel será como abaixo
+qtde_funcionarios = int(input("Digite a quantidade de funcionários: "))
+media_mensalidade = float(input("Digite a média da mensalidade: "))
+print("==============================================================")
+#para concatenar String usar o +
+print(nome + " trabalha na empresa " + empresa)
+#para concatenar valor ou metodo usar ,
+print("Possui: ", qtde_funcionarios, " funcionarios.")
+print("A média da mensalidade é de: " + str(media_mensalidade))
+print("==================Verifique os tipos de dados abaixo==================")
+print("O tipo da variavel [nome] é: ", type(nome))
+print("O tipo da variavel [empresa] é: ", type(empresa))
+print("O tipo da variavel [qtde_funcionarios] é: ", type(qtde_funcionarios))
+print("O tipo da variavel [media_mensalidade] é: ", type(media_mensalidade))
