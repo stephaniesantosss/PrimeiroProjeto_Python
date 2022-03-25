efae98ed004e2596c694f569bc642223da3c9975
stephaniesantosss <stephanieps2016@hotmail.com>
2022-03-25 14:42:49 -0300

    Trabalhando com json

--- Manipulacao_Arquivos/arquivo.py
+++ Manipulacao_Arquivos/arquivo.py
@@ -1,9 +1,38 @@
-basedados = []
+# basedados = []
+#
+# with open("iris.data", "r") as arquivo:
+#     for registro in arquivo.readlines():
+#         basedados.append(registro.split(","))
+#
+# print(basedados)
+#
+# print(float(basedados[0][0]) + float(basedados[0][1]))
 
-with open("iris.data", "r") as arquivo:
-    for registro in arquivo.readlines():
-        basedados.append(registro.split(","))
+# lendo um arquivo json ##########################
+#
+# import json
+#
+# with open("bd.json", "r") as arquivoJson:
+#     dic = json.load(arquivoJson)
+#     for chave, dados in dic.items():
+#         print(chave + " " + str(dados))
 
-print(basedados)
+#### criando um arquivo json ################
 
-print(float(basedados[0][0]) + float(basedados[0][1]))
+import json
+
+dicionario = {
+    "STEPHANIE": [
+        "STEPHANIE SANTOS",
+        "14/09/1998",
+        "TI"
+    ],
+    "JHONY": [
+        "JHONY SANTOS",
+        "22/08/1990",
+        "TI"
+    ]
+}
+
+with open("bd1.json", "w") as arquivoJson:
+    json.dump(dicionario, arquivoJson)
